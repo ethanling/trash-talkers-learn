6e0f40b4f5c433b10d06cb71291dbe1dd42d12c2
Ethan Ling <ethanling1@gmail.com>
2019-12-10 03:31:46 +0000

    Card styling

--- src/components/ScrollView.js
+++ src/components/ScrollView.js
@@ -5,37 +5,37 @@ const testArr = [
     {
         title: "Video 1",
         desciption: "A cool video that is cool.",
-        thumbnail_link: "https://via.placeholder.com/150"
+        thumbnail_link: "https://via.placeholder.com/200"
     },
     {
-        title: "Video 1",
+        title: "Video 2",
         desciption: "A cool video that is cool.",
-        thumbnail_link: "https://via.placeholder.com/150"
+        thumbnail_link: "https://via.placeholder.com/200"
     },
     {
-        title: "Video 1",
+        title: "Video 3",
         desciption: "A cool video that is cool.",
-        thumbnail_link: "https://via.placeholder.com/150"
+        thumbnail_link: "https://via.placeholder.com/200"
     },
     {
-        title: "Video 1",
+        title: "Video 4",
         desciption: "A cool video that is cool.",
-        thumbnail_link: "https://via.placeholder.com/150"
+        thumbnail_link: "https://via.placeholder.com/200"
     },
     {
-        title: "Video 1",
+        title: "Video 5",
         desciption: "A cool video that is cool.",
-        thumbnail_link: "https://via.placeholder.com/150"
+        thumbnail_link: "https://via.placeholder.com/200"
     },
     {
-        title: "Video 1",
+        title: "Video 6",
         desciption: "A cool video that is cool.",
-        thumbnail_link: "https://via.placeholder.com/150"
+        thumbnail_link: "https://via.placeholder.com/200"
     },
     {
-        title: "Video 1",
+        title: "Video 7",
         desciption: "A cool video that is cool.",
-        thumbnail_link: "https://via.placeholder.com/150"
+        thumbnail_link: "https://via.placeholder.com/200"
     }
 ];
 
@@ -63,24 +63,23 @@ const StyledCard = styled.div`
 	display: inline-block;
     justify-content: center;
     align-items: center;
-	height: 200px;
 	width: 200px;
-	background-color: #303030;
 	margin: 0 1em 0 0;
-    box-shadow: 0 0 15px -5px #111;
 `;
 
 const StyledCardWrapper = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-items: center;
+`;
+
+const StyledCardImg = styled.img`
+    box-shadow: 0 0 15px 0px #000;	
 `;
 
 const StyledCardTitle = styled.h3`
-    margin: 0;
+    margin: .5em 0 .5em 0;
     padding: 0;
-    font-size: .8em;
+    font-size: 1em;
 `;
 
 const StyledCardDescription = styled.h3`
@@ -93,7 +92,7 @@ const Card = ({ title, description, thumbnail_link }) => {
 	return (
         <StyledCard>
             <StyledCardWrapper>
-                <img src={thumbnail_link} alt={title} />
+				<StyledCardImg src={thumbnail_link} alt={title} />
                 <StyledCardTitle>{title}</StyledCardTitle>
                 <StyledCardDescription>{description}</StyledCardDescription>
             </StyledCardWrapper>
