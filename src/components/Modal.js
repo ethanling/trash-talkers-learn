f661f78644c32db73cdb75e30f78e081f75ac074
Ethan Ling <ethanling1@gmail.com>
2019-12-16 15:23:28 +0000

    update

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,13 +2,38 @@ import React from 'react';
 import styled from 'styled-components';
 import { useSpring, animated } from 'react-spring';
 
-const StyledShade = styled.div`
+const StyledWrapper = styled.div`
+	z-index: 8;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    flex-direction: column;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    transition: all 0.2 ease;
+`;
+
+
+
+const Modal = ({ children, toggle, action }) => {
+    const growHeight = useSpring({
+        height: toggle ? ('100vh') : ('0vh')
+    })
+
+    const fade = useSpring({
+        opacity: toggle ? ('1') : ('0')
+    })
+
+const StyledShade = styled(animated.div)`
     width: 100%;
     flex: 1;
     background: rgba(0, 0, 0, .9);
 `;
 
-const StyledModalWrapper = styled.div`
+const StyledModalWrapper = styled(animated.div)`
     z-index: 9;
     width: 100vw;
     background-color: #1c1c1c;
@@ -18,30 +43,13 @@ const StyledModalWrapper = styled.div`
     padding: 40px 0 100px 0;
     border-top: 1px solid #000;
 `;
+	
 
-const Modal = ({ children, toggle, action }) => {
-    const fade = useSpring({
-        height: toggle ? ('100vh') : ('0vh')
-    })
-
-    const StyledWrapper = styled(animated.div)`
-        z-index: 8;
-        height: 100vh;
-        width: 100vw;
-        display: flex;
-        justify-content: center;
-        align-items: flex-end;
-        flex-direction: column;
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        transition: all 0.2 ease;
-    `;
 
     return (
-        <StyledWrapper style={fade}>
-            <StyledShade onClick={action} />
-            <StyledModalWrapper>{children}</StyledModalWrapper>
+        <StyledWrapper>
+            <StyledShade onClick={action} style={fade}/>
+            <StyledModalWrapper style={growHeight}>{children}</StyledModalWrapper>
         </StyledWrapper>
     );
 }
